12102df5beecaa28ff6d26f7a3b3de642889f0ff
Yuvraj Tak <yuvrajtak17@gmail.com>
2021-07-15 22:26:04 +0530

    E-Commerce Shopping App. Best for both Shopkeeper(Admins) and Consumers(Users).

---
+++ app/src/main/java/com/e_commerce/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.e_commerce.shopping.Prevalent;
+
+import com.e_commerce.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
